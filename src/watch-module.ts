a7032abe79153661355115ed8c8ef738817eb9c9
Thorsten Lorenz <thlorenz@gmx.de>
2021-10-12 15:53:55 -0500

    code: basic implementation worked out

---
+++ src/watch-module.ts
@@ -0,0 +1,98 @@
+import EventEmitter from 'events'
+import Module from 'module'
+
+// -----------------
+// Emitter
+// -----------------
+class ModuleWatcher extends EventEmitter {
+  constructor(private readonly _filter: LoadedModuleFilter) {
+    super()
+  }
+
+  private _matchesFilter(info: LoadedModuleInfo) {
+    if (info.isCoreModule && !this._filter.coreModules) return false
+    if (info.isNodeModule && !this._filter.nodeModules) return false
+    return this._filter.userModules
+  }
+
+  static registeredWatchers: Set<ModuleWatcher> = new Set()
+
+  static onModuleLoaded(nodeModule: LoadedModuleInfo) {
+    for (const watcher of this.registeredWatchers) {
+      if (watcher._matchesFilter(nodeModule)) {
+        watcher.emit('match', nodeModule)
+      }
+    }
+  }
+}
+
+// -----------------
+// Module._load override
+// -----------------
+// @ts-ignore
+const origLoad = Module._load
+// @ts-ignore
+Module._load = function (
+  moduleUri: string,
+  parent: NodeModule | undefined,
+  isMain: boolean
+) {
+  const parentUri = parent?.id ?? '<root>'
+  const isCoreModule = isCore(moduleUri)
+  const isNodeModule = isNodeMod(moduleUri)
+  ModuleWatcher.onModuleLoaded({
+    moduleUri,
+    parentUri,
+    isCoreModule,
+    isNodeModule,
+  })
+  return origLoad.apply(Module, [moduleUri, parent, isMain])
+}
+
+// -----------------
+// API
+// -----------------
+export function addWatcher(filter: LoadedModuleFilter) {
+  verifyFilter(filter)
+  const moduleWatcher = new ModuleWatcher(filter)
+  ModuleWatcher.registeredWatchers.add(moduleWatcher)
+  return moduleWatcher
+}
+
+export function removeWatcher(moduleWatcher: ModuleWatcher) {
+  return ModuleWatcher.registeredWatchers.delete(moduleWatcher)
+}
+
+function verifyFilter(filter: LoadedModuleFilter) {
+  if (!filter.coreModules && !filter.nodeModules && !filter.userModules) {
+    throw new Error(
+      `Invalid filter ${filter}, at least one prop needs to be true`
+    )
+  }
+}
+
+export type LoadedModuleInfo = {
+  moduleUri: string
+  parentUri: string
+  isCoreModule: boolean
+  isNodeModule: boolean
+}
+
+export type LoadedModuleFilter = {
+  coreModules: boolean
+  nodeModules: boolean
+  userModules: boolean
+}
+
+// -----------------
+// Matchers
+// -----------------
+const coreModules = new Set(Module.builtinModules)
+function isCore(uri: string) {
+  return coreModules.has(uri)
+}
+
+const NODE_MODULE_RX = /node_modules\/.+/
+function isNodeMod(uri: string) {
+  return NODE_MODULE_RX.test(uri)
+}
